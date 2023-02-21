a489d72be6147695ac62e4bac3ebfa6a254d9561
Codevendor <adam@codevendor.com>
2023-02-21 06:39:49 -0700

    Move verboseMode to variable instead of private

--- src/core/curry-console.js
+++ src/core/curry-console.js
@@ -446,7 +446,7 @@ export class curryConsole extends curryEventEmitter {
             const cb = () => {
 
                 // Write
-                if (this.#verboseMode) this.#console[logType](...outputArgs, profile.output + debugInfo.output);
+                if (verboseMode) this.#console[logType](...outputArgs, profile.output + debugInfo.output);
 
             };
             tempArgs.push(cb);
@@ -456,7 +456,7 @@ export class curryConsole extends curryEventEmitter {
         }
 
         // Write 
-        if (this.#verboseMode) this.#console[logType](...outputArgs, profile.output + debugInfo.output);
+        if (verboseMode) this.#console[logType](...outputArgs, profile.output + debugInfo.output);
 
     }
 
